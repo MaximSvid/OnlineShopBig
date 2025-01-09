ff9ab18e4c43acae704fbf88d1fc239e64d3e32c
Maxim Svidrak <maximsvid@gmail.com>
2025-01-09 16:21:06 +0100

    delivery weiter...

--- MyOnlineShop/ViewModel/DeliveryUserInfoViewModel.swift
+++ MyOnlineShop/ViewModel/DeliveryUserInfoViewModel.swift
@@ -71,4 +71,61 @@ class DeliveryUserInfoViewModel: ObservableObject {
         street = ""
     }
     
+    func observeDeliveryUserInfo() {
+        deliveryUserInfoRepo.observeDeliveryUserInfo() {result in
+            switch result {
+            case .success(let deliveryUserInfo):
+                self.deliveryUserInfo = deliveryUserInfo
+            case .failure(let error):
+                print("Error observing delivery user info: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func prepareForEdit(_ deliveryUserInfo: DeliveryUserInfo) {
+        self.selectedDeliveryUserInfo = deliveryUserInfo
+        self.firstName = deliveryUserInfo.firstName
+        self.lastName = deliveryUserInfo.lastName
+        self.email = deliveryUserInfo.email
+        self.phoneNumber = deliveryUserInfo.phoneNumber
+        self.countryCode = deliveryUserInfo.countryCode
+        self.country = deliveryUserInfo.country
+        self.index = deliveryUserInfo.index
+        self.city = deliveryUserInfo.city
+        self.street = deliveryUserInfo.street
+        self.houseNumber = deliveryUserInfo.houseNumber
+        self.apartmentNumber = deliveryUserInfo.apartmentNumber
+    }
+    
+    func updateDeliveryUserInfo() {
+        guard let existionDeliveryUserInfo = selectedDeliveryUserInfo,
+        let deliveryUserInfoId = existionDeliveryUserInfo.id else {
+            print("No deliveryUserInfoId for update")
+            return
+        }
+        let updatedDeliveryUserInfo = DeliveryUserInfo(
+            id: deliveryUserInfoId,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            countryCode: countryCode,
+            phoneNumber: phoneNumber,
+            country: country,
+            index: index,
+            city: city,
+            street: street,
+            houseNumber: houseNumber,
+            apartmentNumber: apartmentNumber
+        )
+        do {
+            try deliveryUserInfoRepo.updateDeliveryUserInfo(deliveryUserInfo: updatedDeliveryUserInfo)
+            print("DeliveryUserInfo updated successfully")
+            //update im local array
+            if let index = deliveryUserInfo.firstIndex(where: { $0.id == deliveryUserInfoId }) {
+                deliveryUserInfo[index] = updatedDeliveryUserInfo
+            }
+        } catch {
+            print("Error updating deliveryUserInfo: \(error)")
+        }
+    }
 }
